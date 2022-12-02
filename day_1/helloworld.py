a005d1c78cc5df7edff8a9b7d3f5bcad49f90daf
Champi31 <r3r3eydeitaca@gmail.com>
2022-12-02 11:14:47 +0100

    guardar

---
+++ day_1/helloworld.py
@@ -0,0 +1,37 @@
+print (3+4)
+print (3-4)
+print (3*4)
+print (3%4)
+print (3/4)
+print (3**4)
+print (3//4)
+
+print ('Ulises')
+print ('Plata')
+print ('Spain')
+print ('I enjoying 30 days of python')
+
+print (type(10))
+print (type(9.8))
+print (type(3.14))
+print (type(4-4j))
+print (type(['Asanabeth', 'Pyhton', 'Finland']))
+print (type('Ulises'))
+print (type('Plata'))
+print (type('Spain'))
+
+print (3-3)
+print (5/6)
+print (7-7j)
+print ('cacahuete')
+True # Is the cat sleep? If it it's sleep, then the value is True
+print ('Earth', 'Jupiter', 'Neptune', 'Mars', 'Venus', 'Saturn', 'Uranus', 'Mercury') # planets
+print ({2, 4, 5, 6})
+print (['cucumber', 'potato', 'tomato'])
+ 
+print ({'first_name':'Ulises',
+'last_name':'Plata',
+'country':'Spain',
+'age':16,
+'is_married':False,
+'skills':['Guitar', 'Music', 'Videogames']})
