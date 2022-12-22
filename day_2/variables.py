4b28207f938ebc7ded8eb39ab1594ad366639706
Champi31 <r3r3eydeitaca@gmail.com>
2022-12-22 09:14:44 +0100

    Mates

---
+++ day_2/variables.py
@@ -0,0 +1,45 @@
+print('Day 2: 30 Days of python programming')
+first_name = 'Ulises'
+last_name = 'Plata' 
+full_name = 'Ulises Plata Piñero'
+country = 'Spain'
+city = 'Jerez de la Frontera'
+age = '16'
+year = '2006'
+is_married = False
+is_true = 'The birds can flying'
+is_light_on = 'Yes'
+hoobie, favorite_food, horoscope = 'Play de guitar', 'Pork cheek', 'Aries'
+
+print (type(first_name))
+print (type(last_name))
+print (type(full_name))
+print (type(country))
+print (type(city))
+print (type(age))
+print (type(year))
+print (type(is_married))
+print (type(is_true))
+print (type(is_light_on))
+print (type(hoobie))
+print (type(favorite_food))
+print (type(favorite_food))
+print (type(horoscope))
+
+print (len(first_name))
+print (len(first_name), len(last_name))
+
+num_one = 5
+num_two = 4
+total=(num_one + num_two) 
+diff=(num_one - num_two)
+product=(num_one*num_two)
+division=(num_one/num_two)
+remainder=(num_one%num_two)
+exp=(num_one**num_two)
+floor_division=(num_one//num_two)
+
+area_of_circle = ((3.1416*30)**2)
+circum_of_circle = (3.1416*60)
+input (30)
+print ((3.1416*30)**2)
